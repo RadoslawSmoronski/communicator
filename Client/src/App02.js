90838bb240625d08371d98b42e7c2aee4d0a27f1
bhandziak <bartek.handziak@gmail.com>
2024-07-22 15:36:02 +0200

    modified:   Client/src/App.scss 	new file:   Client/src/App02.js 	new file:   Client/src/App02.scss 	modified:   Client/src/index.js

---
+++ Client/src/App02.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+class App02 extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: "",
+            password: ""
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.submitLogin = this.submitLogin.bind(this);
+    }
+
+    handleChange(event){
+        let usernameVal = document.getElementById("username").value;
+        let passwordVal = document.getElementById("password").value;
+        this.setState({username: usernameVal, password: passwordVal});
+    }
+
+    async submitLogin(){
+        console.log(this.state.username, this.state.password)
+        //fetch
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                username: this.state.username,
+                password: this.state.password
+            })
+        };
+
+        const response = await fetch('http://localhost:5205/', requestOptions);
+        const data = await response.json();
+        console.log(data);
+
+        this.setState({username: "", password: ""});
+    }
+
+    render(){
+        
+        return (
+            <div id="formBody">
+            <label htmlFor="username">Username: </label>
+            <input name="username" id="username" type="text" value={this.state.username} onChange={this.handleChange}/><br/><br/>
+            <label htmlFor="password">Password: </label>
+            <input name="password" id="password" type="password" value={this.state.password} onChange={this.handleChange}/><br/>
+            <button onClick={this.submitLogin}>Login</button>
+            </div>
+        );
+    }
+}
+export default App02;
--- Client/src/index.js
+++ Client/src/index.js
@@ -4,6 +4,9 @@ import { createRoot } from 'react-dom/client';
 import App from "./App";
 import './App.scss';
 
+import App02 from "./App02";
+import './App02.scss';
+
 const appElement = document.getElementById('main');
 const root = createRoot(appElement);
-root.render(<App />);
+root.render(<App02 />);
