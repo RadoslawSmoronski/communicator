67d112e6c84c60fad0778c10d8be18cba785cc5e
RadoslawSmoronski <radoslaw.smo@gmail.com>
2024-08-01 17:20:29 +0200

    Created Register_ShouldReturnTrue_WhenCalledWithValidParameters test

--- Api.Tests/Controllers/UserControllerTest.cs
+++ Api.Tests/Controllers/UserControllerTest.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using FakeItEasy;
+using FluentAssertions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Api.Models;
+using Api.Controllers;
 
 namespace Api.Tests.Controllers
 {
     public class UserControllerTest
     {
+        [Fact]
+        public void Register_ShouldReturnTrue_WhenCalledWithValidParameters()
+        {
+            // Arrange
+            var userController = A.Fake<UserController>();
+            var registerDto = new RegisterDto(UserName: "login", Password: "password123");
+
+            // Act
+            var result = userController.Register(registerDto);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType<RegisterDto>();
+            result.Should().Be().EquivalentTo(registerDto);
+        }
     }
 }
---
+++ Api/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Api.Models;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/user")]
+    [ApiController]
+    public class UserController : Controller
+    {
+        private UserManager<UserAccount> _userManager;
+
+        public UserController(UserManager<UserAccount> userManager)
+        {
+            _userManager = userManager;
+        }
+
+    }
+}
