4ba17e0955696f64e6a8a7c36cfc6c0e81d4a943
RadoslawSmoronski <radoslaw.smo@gmail.com>
2024-09-25 14:05:53 +0200

    Creaded FriendShip and PendingFriendship models

---
+++ Api/Api.Models/FriendShip/FriendShip.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Models.FriendShip
+{
+    public class FriendShip
+    {
+        public int Id { get; set; }
+
+        public string User1Id { get; set; } = string.Empty;
+        public string User2Id { get; set; } = string.Empty;
+
+        public UserAccount User1 { get; set; } = new UserAccount();
+        public UserAccount User2 { get; set; } = new UserAccount();
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+    }
+}
