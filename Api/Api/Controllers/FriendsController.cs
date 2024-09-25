6929e2c76c53a6c7316055a0a21420642526657b
RadoslawSmoronski <radoslaw.smo@gmail.com>
2024-09-25 16:08:09 +0200

    Created FriendsController

---
+++ Api/Api/Controllers/FriendsController.cs
@@ -0,0 +1,22 @@
+﻿using Api.Data.IRepository;
+using Api.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/friends")]
+    [ApiController]
+    public class FriendsController : Controller
+    {
+        private readonly UserManager<UserAccount> _userManager;
+        private readonly IPendingFriendshipRepository _pendingFriendshipRepository;
+
+        public FriendsController(UserManager<UserAccount> userManager, IPendingFriendshipRepository pendingFriendshipRepository)
+        {
+            _userManager = userManager;
+            _pendingFriendshipRepository = pendingFriendshipRepository;
+        }
+
+    }
+}
