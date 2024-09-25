44b016d8ddad91f2d9c4e3971fddaf512b89a4ea
RadoslawSmoronski <radoslaw.smo@gmail.com>
2024-09-25 16:17:40 +0200

    Created beta version of SendInvite api

--- Api/Api/Controllers/FriendsController.cs
+++ Api/Api/Controllers/FriendsController.cs
@@ -1,5 +1,6 @@
 ﻿using Api.Data.IRepository;
 using Api.Models;
+using Api.Models.Dtos.Controllers.FriendsController;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -18,5 +19,26 @@ namespace Api.Controllers
             _pendingFriendshipRepository = pendingFriendshipRepository;
         }
 
+
+        [HttpPost("sendInviteAsync")]
+        public async Task<IActionResult> SendInviteAsync(SendInviteDto sendInviteDto)
+        {
+            if(ModelState.IsValid == false)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                await _pendingFriendshipRepository.SendInviteAsync(sendInviteDto.SenderId, sendInviteDto.RecipientId);
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
     }
 }
