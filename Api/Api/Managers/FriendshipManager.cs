5c4615d70a636ebd4a893f7f5523a92f90318127
RadoslawSmoronski <radoslaw.smo@gmail.com>
2024-09-26 14:11:47 +0200

    Added fundamental code in FriendshipManager

--- Api/Api/Managers/FriendshipManager.cs
+++ Api/Api/Managers/FriendshipManager.cs
@@ -1,10 +1,18 @@
-﻿using Api.Managers.IManager;
+﻿using Api.Data.IRepository;
+using Api.Managers.IManager;
 using Api.Models.Dtos.Managers.FriendshipManager.SendInviteAsync;
 
 namespace Api.Managers
 {
     public class FriendshipManager : IFriendshipManager
     {
+        private readonly IPendingFriendshipRepository _pendingFriendshipRepository;
+
+        public FriendshipManager(IPendingFriendshipRepository pendingFriendshipRepository)
+        {
+            _pendingFriendshipRepository = pendingFriendshipRepository;
+        }
+
         public async Task<SendInviteResultDto> SendInviteAsync(string senderId, string recipientId)
         {
             var result = new SendInviteResultDto
