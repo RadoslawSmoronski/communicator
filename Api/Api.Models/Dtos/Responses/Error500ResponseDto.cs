818354e8b9f0536b6164fc41e60c19b232aef7da
RadoslawSmoronski <radoslaw.smo@gmail.com>
2024-10-04 11:41:48 +0200

    Fixed Error500ResponseDto object

--- Api/Api.Models/Dtos/Responses/Error500ResponseDto.cs
+++ Api/Api.Models/Dtos/Responses/Error500ResponseDto.cs
@@ -7,12 +7,11 @@ using System.Threading.Tasks;
 
 namespace Api.Models.Dtos.Responses
 {
-    public class Error500ResponseDto : IErrorResponseDto, IErrorResponseWithErrorsDto
+    public class Error500ResponseDto : IErrorResponseDto
     {
         public string Type { get; } = "https://tools.ietf.org/html/rfc9110#section-15.6.1";
         public string Title { get; set; } = String.Empty;
         public int Status { get; } = 500;
-        public IDictionary<string, IEnumerable<string>> Errors { get; set; } = new Dictionary<string, IEnumerable<string>>();
         public string? TraceId { get; set; }
     }
 }
