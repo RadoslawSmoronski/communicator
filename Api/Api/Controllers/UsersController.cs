40a7d22c22271d0a9af92e85141da792e4bbfd12
RadoslawSmoronski <radoslaw.smo@gmail.com>
2024-11-14 16:00:35 +0100

    Refectored getUsersByText

--- Api/Api/Controllers/UsersController.cs
+++ Api/Api/Controllers/UsersController.cs
@@ -76,60 +76,59 @@ namespace Api.Controllers
             }
         }
 
-        //[HttpGet("getUsersByText/{text}")]
+        [HttpGet("getUsersByText/{text}")]
         //[Authorize]
-        //public async Task<IActionResult> getUsersByTextAsync([FromRoute] string text)
-        //{
-        //    if (string.IsNullOrWhiteSpace(text))
-        //    {
-        //        return BadRequest(new GetUsersResponseFailedDto()
-        //        {
-        //            Succeeded = false,
-        //            Message = "Input value is empty."
-        //        });
-        //    }
-            
-        //    if(text.Length > 25 || text.Length < 3)
-        //    {
-        //        return BadRequest(new GetUsersResponseFailedDto()
-        //        {
-        //            Succeeded = false,
-        //            Message = "Username must be more than 3 characters and less than 25."
-        //        });
-        //    }
+        public async Task<IActionResult> getUsersByTextAsync([FromRoute] string text)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                var response = _responseHttpFactory.Create
+                               (ResponseHttpType.BadRequest,
+                               "Input value is empty.");
 
-        //    try
-        //    {
-        //        var users = await _userManager.Users
-        //            .Where(x => x.UserName!.Contains(text))
-        //            .ToListAsync();
+                return BadRequest(response);
+            }
 
-        //        if (users == null || users.Count == 0)
-        //        {
-        //            return NotFound(new GetUsersResponseFailedDto()
-        //            {
-        //                Succeeded = false,
-        //                Message = "There is no user with this username."
-        //            });
-        //        }
+            if (text.Length > 25 || text.Length < 3)
+            {
+                var response = _responseHttpFactory.Create
+                               (ResponseHttpType.BadRequest,
+                               "Username must be more than 3 characters and less than 25.");
 
-        //        var userDtos = _mapper.Map<List<GetUsersUserResponseDto>>(users);
+                return BadRequest(response);
+            }
 
-        //        return Ok(new GetUsersResponseOkDto()
-        //        {
-        //            Succeeded = true,
-        //            Message = "Users were found successfully.",
-        //            Users = userDtos
-        //        });
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        return StatusCode(500, new GetUsersResponseFailedDto
-        //        {
-        //            Succeeded = false,
-        //            Message = "An internal server error occurred."
-        //        });
-        //    }
-        //}
+            try
+            {
+                var users = await _userManager.Users
+                    .Where(x => x.UserName!.Contains(text))
+                    .ToListAsync();
+
+                if (users == null || users.Count == 0)
+                {
+                    var response = _responseHttpFactory.Create
+                                   (ResponseHttpType.NotFound,
+                                   "There is no user with this username.");
+
+                    return NotFound(response);
+                }
+
+                var userDtos = _mapper.Map<List<UsersDto>>(users);
+
+                var responseOk = _responseHttpFactory.Create<List<UsersDto>>
+                              (ResponseHttpType.Success,
+                              "There is no user with this username.",
+                              userDtos);
+
+                return Ok(responseOk);
+            }
+            catch (Exception ex)
+            {
+                var response = _responseHttpFactory.Create
+                               (ResponseHttpType.InternalServerError, "An internal server error occurred.");
+
+                return StatusCode(500, response);
+            }
+        }
     }
 }
