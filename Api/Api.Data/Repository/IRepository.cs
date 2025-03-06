3371093561e2d89d2e7e1abe303eae08165fdcc6
RadoslawSmoronski <radoslaw.smo@gmail.com>
2025-03-06 16:45:04 +0100

    Add FirstOrDefaultAsync to Repository

--- Api/Api.Data/Repository/IRepository.cs
+++ Api/Api.Data/Repository/IRepository.cs
@@ -5,9 +5,9 @@ namespace Api.Data.Repository
     public interface IRepository<T> where T : class
     {
         Task<IEnumerable<T>> GetAllAsync();
-        Task<T> GetByIdAsync(int id);
         Task<bool> AnyAsync(Expression<Func<T, bool>> predicate);
         Task<IEnumerable<T>> WhereAsync(Expression<Func<T, bool>> predicate);
+        Task<T?> FirstOrDefaultAsync(Expression<Func<T, bool>> predicate);
         Task AddAsync(T entity);
         void Update(T entity);
         void Delete(T entity);
--- Api/Api.Data/Repository/Repository.cs
+++ Api/Api.Data/Repository/Repository.cs
@@ -33,6 +33,11 @@ public class Repository<T> : IRepository<T> where T : class
         return await _dbSet.Where(predicate).ToListAsync();
     }
 
+    public async Task<T?> FirstOrDefaultAsync(Expression<Func<T, bool>> predicate)
+    {
+        return await _dbSet.FirstOrDefaultAsync(predicate);
+    }
+
     public async Task AddAsync(T entity)
     {
         await _dbSet.AddAsync(entity);
