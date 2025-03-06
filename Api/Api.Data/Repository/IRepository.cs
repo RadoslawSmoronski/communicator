ca3dabc65a7c8324ad5e490b177a7587f6946361
RadoslawSmoronski <radoslaw.smo@gmail.com>
2025-03-06 15:48:58 +0100

    Add AnyAsync method to Repository

--- Api/Api.Data/Repository/IRepository.cs
+++ Api/Api.Data/Repository/IRepository.cs
@@ -1,9 +1,12 @@
-﻿namespace Api.Data.Repository
+﻿using System.Linq.Expressions;
+
+namespace Api.Data.Repository
 {
     public interface IRepository<T> where T : class
     {
         Task<IEnumerable<T>> GetAllAsync();
         Task<T> GetByIdAsync(int id);
+        Task<bool> AnyAsync(Expression<Func<T, bool>> predicate);
         Task AddAsync(T entity);
         void Update(T entity);
         void Delete(T entity);
