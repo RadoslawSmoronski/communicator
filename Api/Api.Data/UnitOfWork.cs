65562fce1c5c607a2c0ad97eba1c49f5ad8c1c00
RadoslawSmoronski <radoslaw.smo@gmail.com>
2025-03-06 13:34:43 +0100

    Add files for UnityOfWork pattern and IRepository interface

---
+++ Api/Api.Data/UnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿namespace Api.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> SaveAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
