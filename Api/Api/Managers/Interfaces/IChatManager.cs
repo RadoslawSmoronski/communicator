8e239fe0e5039f911f1871b8eb12e2cdf88bdde2
RadoslawSmoronski <radoslaw.smo@gmail.com>
2025-03-13 13:17:31 +0100

    Add Conversation and Message to IUnitOfWork and create IChatManager.

---
+++ Api/Api/Managers/Interfaces/IChatManager.cs
@@ -0,0 +1,21 @@
+﻿using Api.Models.Chat;
+using Api.Utilities.Result;
+
+namespace Api.Managers.Interfaces
+{
+    public interface IChatManager
+    {
+        //Conversation
+        ResultT<Conversation> GetOrCreateConversation(string friendId);
+
+        //Message
+        //ResultT<bool> SendMessage(string conversationId, string message);
+        //ResultT<List<Message>> GetMessages(string conversationId, int amount, string? beforeMessageId);
+        //ResultT<List<Message>> GetUnreadMessages();
+        //ResultT<Message> GetLastMessage();
+
+        ////GetUserStatus(string userId);
+        ////Result MarkMessagesAsRead(Guid conversationId, List<Guid> messageIds);
+        ////ResultT<List<Conversation>> GetConversations(int amount, int offset);
+    }
+}
