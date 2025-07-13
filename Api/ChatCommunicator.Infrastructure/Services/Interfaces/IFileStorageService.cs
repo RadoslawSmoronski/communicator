c97f77cfa768dc2f3f158fd757c9927f4dfe133e
RadoslawSmoronski <radoslaw.smo@gmail.com>
2025-07-13 17:11:44 +0200

    Create FileStorage Service.

---
+++ Api/ChatCommunicator.Infrastructure/Services/Interfaces/IFileStorageService.cs
@@ -0,0 +1,11 @@
+﻿using ChatCommunicator.Shared.Result;
+using Microsoft.AspNetCore.Http;
+
+namespace ChatCommunicator.Infrastructure.Services.Interfaces
+{
+    public interface IFileStorageService
+    {
+        Task<Result> SaveFileAsync(IFormFile file, string directory, string filename);
+        Result DeleteFile(string directory, string filename);
+    }
+}
