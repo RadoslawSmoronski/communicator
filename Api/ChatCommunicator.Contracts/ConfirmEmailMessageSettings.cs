189f03ca73347dac40d506e0ee5d482a851c34e5
RadoslawSmoronski <radoslaw.smo@gmail.com>
2025-09-10 17:00:12 +0200

    Add title field in confimEmailMessageSettings and add urlEncode for token.

--- Api/ChatCommunicator.Contracts/ConfirmEmailMessageSettings.cs
+++ Api/ChatCommunicator.Contracts/ConfirmEmailMessageSettings.cs
@@ -2,6 +2,7 @@
 {
     public class ConfirmEmailMessageSettings
     {
+        public required string Title { get; set; }
         public required string Content { get; set; }
         public required string Address { get; set; }
     }
