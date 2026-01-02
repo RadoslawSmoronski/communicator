999bdb4e8f5f85be137da43dee7e96127bf6e77a
bhandziak <64088278+bhandziak@users.noreply.github.com>
2026-01-02 23:01:20 +0100

    Fixed 401 error after login in

--- src/features/auth/services/authService.js
+++ src/features/auth/services/authService.js
@@ -5,7 +5,10 @@ import { mapLoginResponseToUser } from "../mappers/loginMapper";
 export const loginUser = async (data) => {
     const response = await axios.post(
         APIs.LOGIN,
-        JSON.stringify(data)
+        JSON.stringify(data),
+        {
+            withCredentials: true
+        }
     );
 
     return mapLoginResponseToUser(response.data, data.Email);
